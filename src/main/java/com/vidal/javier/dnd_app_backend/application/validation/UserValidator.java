6fe48c3d3a56a344a7c47394567a0204d31a2630
Javier <javyervg@gmail.com>
2025-03-24 17:52:14 +0100

    fixed password regexp

--- src/main/java/com/vidal/javier/dnd_app_backend/application/validation/UserValidator.java
+++ src/main/java/com/vidal/javier/dnd_app_backend/application/validation/UserValidator.java
@@ -18,7 +18,7 @@ public class UserValidator {
 
     private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
     private static final Pattern PASSWORD_PATTERN = Pattern
-            .compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&])[A-Za-z0-9@$!%*?&]{8,}$");
+            .compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,}$");
 
     private final ImageValidator imageValidator;
 
